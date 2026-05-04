99695a5cc19cafc50fa15983f059468432abd140
Jaime Suarez <jaisua8@gmail.com>
2026-05-03 21:00:57 -0500

    correcion del efecto segun el cambio de id de la incidencia

--- src/app/features/incidencia/detalle-incidencia/detalle-incidencia.ts
+++ src/app/features/incidencia/detalle-incidencia/detalle-incidencia.ts
@@ -1,6 +1,6 @@
 import { Incidencia } from './../../../core/models/incident.model';
-import { Component, inject, input, model, OnInit, signal } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, effect, inject, input, model, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { IncidenciaService } from '@services/incident.service';
 
 @Component({
@@ -11,23 +11,26 @@ import { IncidenciaService } from '@services/incident.service';
 })
 export class DetalleIncidencia implements OnInit {
   modal = model(false);
-  incidenciaId =  model<number |null>();
+  incidenciaId = model<number | null>();
 
-  incidencia = signal< null | Incidencia>(null)
+  incidencia = signal<null | Incidencia>(null);
   incidentService = inject(IncidenciaService);
 
-  ngOnInit(){
-    if(this.incidenciaId() ){
-      const newIncide = this.incidentService.getIncidencia(this.incidenciaId()!);
-      if(newIncide){
-        this.incidencia.set(newIncide);
-        console.log(newIncide);
-
+  constructor() {
+    effect(() => {
+      if (this.incidenciaId()) {
+        const newIncide = this.incidentService.getIncidencia(this.incidenciaId()!);
+        if (newIncide) {
+          this.incidencia.set(newIncide);
+          console.log(newIncide);
+        }
       }
-    }
+    });
+  }
+  ngOnInit(): void {
   }
 
-  openBox(){
-    this.modal.update(m => !m)
+  openBox() {
+    this.modal.update((m) => !m);
   }
 }
