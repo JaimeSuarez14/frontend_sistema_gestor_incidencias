c63fc7f9bfba413fa01d1337ccc1377a820dfd4f
Jaime Suarez <jaisua8@gmail.com>
2026-05-07 19:56:50 -0500

    integracion del componente de busqueda en la seccion de users

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -5,10 +5,10 @@ import { User } from '../models/user.model';
 export class UserService {
   private readonly _users = signal<User[]>([
     { id: "1", name: 'Juan Pérez', email: 'juan.perez@empresa.com', role: 'ADMIN' , estado: "ACTIVO" },
-    { id: "2", name: 'María López', email: 'maria.lopez@empresa.com', role: 'TECNICO_NIVEL_1' ,  estado: "ACTIVO" },
-    { id: "3", name: 'Carlos Ramírez', email: 'carlos.ramirez@empresa.com', role: 'EMPLEADO' ,  estado: "ACTIVO" },
-    { id: "4", name: 'Ana Torres', email: 'ana.torres@empresa.com', role: 'TECNICO_NIVEL_1' ,  estado: "ACTIVO" },
-    { id: "5", name: 'Luis Fernández', email: 'luis.fernandez@empresa.com', role: 'EMPLEADO',  estado: "ACTIVO" },
+    { id: "2", name: 'María López', email: 'maria.lopez@gmail.com', role: 'TECNICO_NIVEL_1' ,  estado: "ACTIVO" },
+    { id: "3", name: 'Carlos Ramírez', email: 'carlos.ramirez@uducad.com', role: 'EMPLEADO' ,  estado: "ACTIVO" },
+    { id: "4", name: 'Ana Torres', email: 'ana.torres@hotmail.com', role: 'TECNICO_NIVEL_1' ,  estado: "ACTIVO" },
+    { id: "5", name: 'Luis Fernández', email: 'luis.fernandez@outlook.com', role: 'EMPLEADO',  estado: "ACTIVO" },
   ]);
 
   readonly users = this._users.asReadonly();
